ac8a6ab4cc1a7180f21dcedd1241ec451889b711
thez3ro <io@thezero.org>
2017-04-02 22:26:46 +0200

    HiB17 Assets

---
+++ assets/hib17_crypto.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import base64
+import sys
+import requests
+import binascii
+from urllib import parse
+
+def block_print(c,size):
+    lenc = len(c)
+    i = 0
+    while i < lenc:
+        print(c[i:i+size])
+        i += size
+
+def decode(c):
+    return binascii.hexlify(base64.b64decode(parse.unquote(c)))
+    
+def encode(c):
+    print(base64.b64encode(binascii.unhexlify(c)))
+    return parse.quote(base64.b64encode(binascii.unhexlify(c)))
+    
+def crypt(payload):
+    post = requests.post('http://url:9090/index.php', data={'cc': payload})
+    c = post.cookies['pci_crypt']
+    resp = requests.get('http://url:9090/index.php', cookies={'pci_crypt': c})
+    s = binascii.hexlify(base64.b64decode(parse.unquote(c)))
+    r = resp.text[:-len(payload)-1]    
+    return s,r
+    
+def decrypt(ciphertext):
+    ciphertext = encode(ciphertext)
+    resp = requests.get('http://url:9090/index.php', cookies={'pci_crypt': ciphertext})
+    print(resp.text)
+    
+c,p = crypt(sys.argv[1])
+ci = bytearray(c)
+block_print(ci,32)
+block_print(p,32)
+# decrypt(ci)
